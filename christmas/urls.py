85f4573fd1da9fdb997ae2cdea215abfb57006d3
Катерина Звіркова <katerinazvirkova@MacBook-Pro-Katerina.local>
2024-09-17 15:05:08 +0300

    changed files

--- christmas/urls.py
+++ christmas/urls.py
@@ -5,5 +5,6 @@ app_name = 'christmas'
 urlpatterns = [
     path('', views.index, name='index'),
     path('secret_santa/', views.secret_santa_home, name='secret_santa_home'),
+
     path('generate_pairs/', views.generate_pairs_view, name='generate_pairs'),
 ]
--- christmas/views.py
+++ christmas/views.py
@@ -2,20 +2,17 @@ from django.shortcuts import render, redirect
 from datetime import datetime
 from random import shuffle
 
-players_list = []
 def index(request):
     today = datetime.now()
-    is_christmas = "Yes!" if today.month == 12 and today.day == 25 else "No!"
+    is_christmas = today.month == 12 and today.day == 25
     return render(request, 'christmas/index.html', {'is_christmas': is_christmas})
 
-def get_players():
-    return players_list
+def get_players(request):
+    return request.session.get('players', [])
 
 def validate_players(players):
     if len(players) < 2:
         return 'Add at least two players'
-    if len(players) % 2 != 0:
-        return 'The number of players must be even'
     return None
 
 def generate_pairs(players):
@@ -30,22 +27,23 @@ def generate_pairs(players):
     return pairs
 
 def secret_santa_home(request):
-    global players_list
-    players = get_players()
+    players = get_players(request)
 
     if request.method == 'POST':
         name = request.POST.get('name')
         if name and name not in players:
             players.append(name)
-            players_list = players
-        elif 'generate_pairs' in request.POST:
-            error = validate_players(players)
-            if error:
-                return render(request, 'christmas/santa.html', {'players': players, 'error': error})
-
-            pairs = generate_pairs(players)
-            players_list = []
-
-            return render(request, 'christmas/result.html', {'pairs': pairs})
+            request.session['players'] = players
 
     return render(request, 'christmas/santa.html', {'players': players})
+
+def generate_pairs_view(request):
+    players = get_players(request)
+    error = validate_players(players)
+
+    if error:
+        return render(request, 'christmas/santa.html', {'players': players, 'error': error})
+    pairs = generate_pairs(players)
+    request.session['players'] = []
+
+    return render(request, 'christmas/result.html', {'pairs': pairs})
