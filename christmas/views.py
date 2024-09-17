5c12dbcd374f44f2c297a86e637691907fb9041e
Катерина Звіркова <katerinazvirkova@MacBook-Pro-Katerina.local>
2024-09-17 14:40:35 +0300

    changed the files

--- christmas/views.py
+++ christmas/views.py
@@ -2,13 +2,14 @@ from django.shortcuts import render, redirect
 from datetime import datetime
 from random import shuffle
 
+players_list = []
 def index(request):
     today = datetime.now()
     is_christmas = "Yes!" if today.month == 12 and today.day == 25 else "No!"
     return render(request, 'christmas/index.html', {'is_christmas': is_christmas})
 
-def get_players(request):
-    return request.session.get('players', [])
+def get_players():
+    return players_list
 
 def validate_players(players):
     if len(players) < 2:
@@ -29,20 +30,21 @@ def generate_pairs(players):
     return pairs
 
 def secret_santa_home(request):
-    players = get_players(request)
+    global players_list
+    players = get_players()
 
     if request.method == 'POST':
         name = request.POST.get('name')
         if name and name not in players:
             players.append(name)
-            request.session['players'] = players
+            players_list = players
         elif 'generate_pairs' in request.POST:
             error = validate_players(players)
             if error:
                 return render(request, 'christmas/santa.html', {'players': players, 'error': error})
 
             pairs = generate_pairs(players)
-            request.session['players'] = []
+            players_list = []
 
             return render(request, 'christmas/result.html', {'pairs': pairs})
 
